f280c41b8504a7c1be1bcad8e96f432dcd71bda3
alessandrodaniele <alessandro.daniele.88@gmail.com>
2022-04-05 11:39:17 +0200

    Removed old printing function

--- example.py
+++ example.py
@@ -1,12 +1,6 @@
 from Godel import *
 from Formula import *
 
-
-def print_values(A, B, V):
-    for a, b, v in zip(A.value, B.value, V):
-        print(str(a[0]) + ', ' + str(b[0]) + ', ' + str(v[0]))
-
-
 A = Predicate('A', torch.tensor([[0.1,0.5,0,0.8]]).t())
 B = Predicate('B', torch.tensor([[0.4,0.2,0.6,0.1]]).t())
 C = Predicate('C', torch.tensor([[0.6,0.1,0.8,0.3]]).t())
