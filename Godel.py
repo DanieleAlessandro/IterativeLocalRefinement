ac99fc95c3be9e5241fe0d2536c4328dd875d561
alessandrodaniele <alessandro.daniele.88@gmail.com>
2022-04-15 15:22:05 +0200

    Added implementation of MAXSAT solver based on LRLBackProp

--- Godel.py
+++ Godel.py
@@ -1,6 +1,6 @@
 import torch
+import numpy as np
 from Formula import Formula
-
 
 class AND(Formula):
     def function(self, truth_values):
@@ -25,6 +25,22 @@ class AND(Formula):
         else:
             return s
 
+    def sat_sub_formulas(self):
+        m = torch.mean(self.input_tensor, 1, keepdim=True)
+        return m.t()
+
+    def count_ones(self):
+        return torch.sum((self.input_tensor == 1.).int(), 1, keepdim=True).float()
+
+    def count_sat(self):
+        return (torch.sum((self.input_tensor == 1.).int(), 1, keepdim=True) == self.input_tensor.shape[1]).float()
+
+    def count_zeros(self):
+        return torch.sum((self.input_tensor == 0.).int(), 1, keepdim=True).float()
+
+    def count_all(self):
+        return torch.tensor(self.input_tensor.shape[1]).float()
+
 
 class OR(Formula):
     def function(self, truth_values):
@@ -33,7 +49,19 @@ class OR(Formula):
         return torch.unsqueeze(m, 0).t()
 
     def boost_function(self, truth_values, delta):
+        # TODO: remove or substitute: here if two values are both the maximum, it choose randomly which one to increase
+        #   - slower
+        #   - it should always reach a solution (if such a solution exists) with w=1 for the SAT problem
+        # indexes = []
+        # for row in truth_values:
+        #     indexes.append(np.random.choice(torch.flatten(torch.argwhere(row == torch.max(row)))))
+        #
+        # # am = torch.argmax(truth_values, 1)
+        # indexes = torch.tensor(np.array(indexes, dtype=np.int64))
+        # return torch.nn.functional.one_hot(indexes, truth_values.shape[1]) * delta
+
         am = torch.argmax(truth_values, 1)
+
         return torch.nn.functional.one_hot(am, truth_values.shape[1]) * delta
 
     def get_name(self, parenthesis=False):
