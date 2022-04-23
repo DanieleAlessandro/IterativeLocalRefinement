fix: Proper computation of mean

SAT_formula.py
@@ -8,6 +8,7 @@ class SATFormula(Formula):
         self.clause_t = None
         formula_tensor = torch.tensor(clauses)
         self.prop_index = formula_tensor.abs() - 1
+        # self.prop_index = formula_tensor.abs()
         self.sign = formula_tensor.sign()
 
     def function(self, truth_values):
@@ -34,8 +35,15 @@ class SATFormula(Formula):
         # Find what propositions the results belong to
         propositions_max_indices = torch.take_along_dim(self.prop_index.unsqueeze(0), max_indices, -1).squeeze()
         # Uses the mean to combine the deltas on the same proposition
-        self.prop_deltas = torch.scatter_reduce(delta_max, -1, propositions_max_indices, "mean")
+        # How to compute the mean on nonzero values: Take the sum (scatter_add), then count amount of nonzero values
+        self.prop_deltas = torch.scatter_reduce(delta_max, -1, propositions_max_indices, 'sum')
 
+        # print(delta_max != 0)
+        amt_nonzero = torch.scatter_reduce((delta_max != 0).float(), -1, propositions_max_indices, 'sum')
+        mask = amt_nonzero > 0
+        self.prop_deltas[mask] = self.prop_deltas[mask] / amt_nonzero[mask]
+
+        # TODO: Max heuristic can be implemented with a max and a min scatter reduce, then finding pointwise absolute largest value
         # print(self.prop_deltas, self.prop_deltas.shape)
 
         return delta_max

logical_layers.py
@@ -15,7 +15,7 @@ class LRL(torch.nn.Module):
             return None
 
         # TODO: What about this weird parameter here? It seems to really smooth out the trajectory
-        delta_sat = torch.where(target - satisfaction > 0, (target - satisfaction).double(), 0.).float() / 1.
+        delta_sat = torch.where(target - satisfaction > 0, (target - satisfaction).double(), 0.).float() / 10.
 
         self.formula.backward(delta_sat)
         delta_tensor = self.formula.get_delta_tensor(truth_values, method)