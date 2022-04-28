4eb4b6334748dc5b366ca58acc005429079ba1f1
Emile <e.van.krieken@vu.nl>
2022-04-28 15:05:14 +0200

    Restructure

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 # Fixed settings
 n_initial_vectors = 10
-n_steps = 100
+n_steps = 1000
 n_formulas = 10
 
 amt_rules = 10 # Max is 91
 
 # Hyper parameters
 tnorm = 'godel'
-targets = [0.3, 0.5, 0.8, 1.0]  # list of target truth values for both LRL and LTN
+targets = [0.5, 0.8, 1.0]  # list of target truth values for both LRL and LTN
 # methods = ['max', 'mean', 'min']  # list of aggregation methods for LRL
 methods = ["mean", "max"]  # list of aggregation methods for LRL
 regularization_lambda_list = [1.0, 0.1, 0.01, 0.0]  # regularization hyperparameter for LTN
