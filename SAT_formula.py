6830b7095a25a1852d0261779bb3f15dc58bcf8b
Emile <e.van.krieken@vu.nl>
2022-04-28 10:32:09 +0200

    Added sgd log-product function

--- SAT_formula.py
+++ SAT_formula.py
@@ -108,12 +108,9 @@ class SATProduct(SATTNorm):
     """
     def forward(self, indexed_t: torch.Tensor) -> torch.Tensor:
         self.indexed_t = indexed_t
-        if torch.isnan(self.indexed_t).any():
-            print("NaN in indexed_t")
         self.clause_t = 1 - torch.prod(1-indexed_t, dim=-1)
         self.formula_t = torch.prod(self.clause_t, dim=-1)
-        if torch.isnan(self.formula_t).any():
-            print("NaN in formula_t")
+
         return self.formula_t
 
     def boost_function(self, delta, prop_index, sign) -> torch.Tensor:
@@ -164,6 +161,14 @@ class SATProduct(SATTNorm):
         mask = amt_nonzero > 0
         prop_deltas[mask] = prop_deltas[mask] / amt_nonzero[mask]
         return prop_deltas
+
+    def sgd_function(self, indexed_t: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        # Compute the t-norm boost function
+        self.clause_t = 1 - torch.prod(1 - indexed_t, dim=-1)
+        self.formula_t = torch.prod(self.clause_t, dim=-1)
+        log_s = torch.log(self.clause_t).sum(dim=-1)
+        return log_s, self.formula_t
+
 
 class SATFormula(Formula):
     def __init__(self, clauses, is_sgd=False, tnorm=SATGodel()):
@@ -221,81 +226,3 @@ class SATFormula(Formula):
     def sat_sub_formulas(self, truth_values):
         return self.tnorm.clause_t
 
-#
-# class Predicate(Formula):
-#     def __init__(self, name, index):
-#         super().__init__(None)
-#         self.name = name
-#         self.index = index
-#         self.deltas = []
-#         self.predicates = [self]
-#
-#     def forward(self, truth_values):
-#         return torch.unsqueeze(truth_values[:, self.index], 1)
-#
-#     def backward(self, delta, randomized=False):  # TODO: implement the usage of randomized
-#         self.deltas.append(delta)
-#
-#     def reset_deltas(self):
-#         self.deltas = []
-#
-#     def aggregate_deltas(self, method='mean') -> Tuple[int, torch.Tensor]:
-#         if method == 'most_clauses':
-#             deltas = torch.concat(self.deltas, 1)
-#             positive = torch.sum(deltas > 0., 1, keepdim=True) - torch.sum(deltas <= 0., 1, keepdim=True) >= 0
-#             max, _ = torch.max(deltas, 1, keepdim=True)
-#             min, _ = torch.min(deltas, 1, keepdim=True)
-#
-#             return self.index, torch.where(positive, max, min)
-#         if method == 'mean':
-#             deltas = torch.concat(self.deltas, 1)
-#
-#             return self.index, torch.nan_to_num(
-#                 torch.sum(deltas, 1, keepdim=True) / torch.sum(deltas != 0.0, 1, keepdim=True))
-#         if method == 'max':
-#             deltas = torch.concat(self.deltas, 1)
-#             abs_deltas = deltas.abs()
-#
-#             i = torch.argmax(abs_deltas, 1, keepdim=True)
-#
-#             return self.index, torch.gather(deltas, 1, i)
-#         if method == 'min':
-#             deltas = torch.concat(self.deltas, 1)
-#             deltas = torch.where(deltas == 0, 100., deltas.double()).float()
-#             abs_deltas = deltas.abs()
-#
-#             i = torch.argmin(abs_deltas, 1, keepdim=True)
-#             final_deltas = torch.gather(deltas, 1, i)
-#
-#             return self.index, torch.where(final_deltas == 100., 0.0, final_deltas.double()).float()
-#         if method == 'randomized_direction':
-#             deltas = torch.concat(self.deltas, 1)
-#             abs_deltas = deltas.abs()
-#
-#             if random.getrandbits(1):
-#                 deltas_no_zeros = torch.where(abs_deltas == 0, 100., abs_deltas.double()).float()
-#                 i = torch.argmin(deltas_no_zeros, 1, keepdim=True)
-#                 final_deltas = torch.gather(deltas, 1, i)
-#
-#                 return self.index, torch.where(final_deltas == 100., 0.0, final_deltas.double()).float()
-#             else:
-#                 i = torch.argmax(abs_deltas, 1, keepdim=True)
-#
-#                 return self.index, torch.gather(deltas, 1, i)
-#
-#     def get_name(self, parenthesis=False):
-#         return self.name
-#
-#
-# class NOT(Formula):
-#     def __init__(self, sub_formula):
-#         super().__init__([sub_formula])
-#
-#     def function(self, truth_values):
-#         return 1 - truth_values
-#
-#     def boost_function(self, truth_values, delta):
-#         return - delta
-#
-#     def get_name(self, parenthesis=False):
-#         return 'NOT(' + self.sub_formulas[0].get_name() + ')'
